6fecee78cf4c63d30f13dbf4445257e789227bac
Carlosnoe10 <carlosdnoem@gmail.com>
2023-09-08 12:53:14 -0700

    INICIO 1

---
+++ src/lab8progra2_carlosnoe/Pais.java
@@ -0,0 +1,11 @@
+package lab8progra2_carlosnoe;
+
+import java.util.ArrayList;
+
+public class Pais {
+    public String Nombre;
+    public ArrayList<Nadadores> Swimer;
+    public int MedallasObtenidas;
+    
+    
+}
