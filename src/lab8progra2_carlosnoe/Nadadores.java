a8c56947d62e0d51321c237afb2439a03d0ebfb4
Carlosnoe10 <carlosdnoem@gmail.com>
2023-09-08 12:59:27 -0700

    Terminading las clases

---
+++ src/lab8progra2_carlosnoe/Nadadores.java
@@ -0,0 +1,85 @@
+package lab8progra2_carlosnoe;
+
+public class Nadadores {
+    public String Nombre;
+    public String Nacionalidad;
+    public int edad;
+    public int DistanciaCompe;
+    public int NumeroDeMedallas;
+    public double Altura;
+    public double MejorTiempo;
+
+    public Nadadores(String Nombre, String Nacionalidad, int edad, int DistanciaCompe, int NumeroDeMedallas, double Altura, double MejorTiempo) {
+        this.Nombre = Nombre;
+        this.Nacionalidad = Nacionalidad;
+        this.edad = edad;
+        this.DistanciaCompe = DistanciaCompe;
+        this.NumeroDeMedallas = NumeroDeMedallas;
+        this.Altura = Altura;
+        this.MejorTiempo = MejorTiempo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getNacionalidad() {
+        return Nacionalidad;
+    }
+
+    public void setNacionalidad(String Nacionalidad) {
+        this.Nacionalidad = Nacionalidad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getDistanciaCompe() {
+        return DistanciaCompe;
+    }
+
+    public void setDistanciaCompe(int DistanciaCompe) {
+        this.DistanciaCompe = DistanciaCompe;
+    }
+
+    public int getNumeroDeMedallas() {
+        return NumeroDeMedallas;
+    }
+
+    public void setNumeroDeMedallas(int NumeroDeMedallas) {
+        this.NumeroDeMedallas = NumeroDeMedallas;
+    }
+
+    public double getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(double Altura) {
+        this.Altura = Altura;
+    }
+
+    public double getMejorTiempo() {
+        return MejorTiempo;
+    }
+
+    public void setMejorTiempo(double MejorTiempo) {
+        this.MejorTiempo = MejorTiempo;
+    }
+
+    @Override
+    public String toString() {
+        return "Nadadores{" + "Nombre=" + Nombre + ", Nacionalidad=" + Nacionalidad + ", edad=" + edad + ", DistanciaCompe=" + DistanciaCompe + ", NumeroDeMedallas=" + NumeroDeMedallas + ", Altura=" + Altura + ", MejorTiempo=" + MejorTiempo + '}';
+    }
+    
+    
+    
+}
--- src/lab8progra2_carlosnoe/Pais.java
+++ src/lab8progra2_carlosnoe/Pais.java
@@ -6,6 +6,41 @@ public class Pais {
     public String Nombre;
     public ArrayList<Nadadores> Swimer;
     public int MedallasObtenidas;
+
+    public Pais(String Nombre, ArrayList<Nadadores> Swimer, int MedallasObtenidas) {
+        this.Nombre = Nombre;
+        this.Swimer = Swimer;
+        this.MedallasObtenidas = MedallasObtenidas;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public ArrayList<Nadadores> getSwimer() {
+        return Swimer;
+    }
+
+    public void setSwimer(ArrayList<Nadadores> Swimer) {
+        this.Swimer = Swimer;
+    }
+
+    public int getMedallasObtenidas() {
+        return MedallasObtenidas;
+    }
+
+    public void setMedallasObtenidas(int MedallasObtenidas) {
+        this.MedallasObtenidas = MedallasObtenidas;
+    }
+
+    @Override
+    public String toString() {
+        return "Pais{" + "Nombre=" + Nombre + ", Swimer=" + Swimer + ", MedallasObtenidas=" + MedallasObtenidas + '}';
+    }
     
     
 }
